fed045cabead568f6df6fc372cff6d48d970cb67
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-05-12 14:06:37 -0300

    agrego clase Game

---
+++ public/js/Game.js
@@ -0,0 +1,19 @@
+class Game {
+    
+    static init() {
+
+    }
+
+    static sendAnswer() {
+        // fetch...
+    }
+
+    static getNewQuestion() {
+        // fetch...
+    }
+
+    static refreshDataGame() {
+        // html...
+    }
+
+}
--- public/js/NewGame.js
+++ public/js/NewGame.js
@@ -1,35 +1,45 @@
 
+const inputUsername = document.querySelector("#inputUsername");
 const btnNewGame = document.querySelector("#btnNewGame")
+const divNewGame = document.querySelector("#new_game")
 
 btnNewGame.addEventListener('click', async (event) => {
     event.preventDefault();
-    const inputUsername = document.querySelector("#inputUsername");
     if (inputUsername.value) {
         const resp = await (await fetch(`/new_game_${inputUsername.value}_topic_${selectTopicGame.value}`)).json();
 
         if (resp.status) {  // todo ok
             HtmlManipulation.showSuccessAlert(resp.message, () => {
-                newGame();
+                newGame(resp.game, inputUsername.value);
             });
         } else {
             HtmlManipulation.showErrorAlert(resp.message)
         }
 
     } else {
-        HtmlManipulation.showErrorAlert("Error: Debe ingresar un nombre de usuario");
+        HtmlManipulation.showErrorAlert("Error: Debe ingresar un nombre de usuario", () => {
+            clearInputUsername();
+        });
+        
     }
 });
 
-function saveNewGameJSON(jsonObject, username) {
-    sessionStorage.setItem("actual_game", JSON.stringify(jsonObject));
-    sessionStorage.setItem("actual_username", JSON.stringify(username))
+function saveNewGameJSON(jsonGameObject, username) {
+    sessionStorage.setItem("actual_game", JSON.stringify(jsonGameObject));
+    sessionStorage.setItem("actual_username", username)
 }
 
-function newGame() {
-    saveNewGameJSON(resp.game, inputUsername.value);
-    inputUsername.value = "";
+const clearInputUsername = ()=> { inputUsername.value = ""; }
+
+function newGame(jsonGameObject, username) {
+    saveNewGameJSON(jsonGameObject, username);
+    clearInputUsername();
 
     console.log("Bienvenido al juego")
+
+    HtmlManipulation.hideHtmlElement(divNewGame);
+    HtmlManipulation.showHtmlElement(document.querySelector("#game"))
+
     /**
      * Show div-game ...
      */

16b4698c9d7c0467b6023241a85f9e94a4da2d70
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-05-11 21:59:22 -0300

    creo estructura básica del juego

--- App.py
+++ App.py
@@ -5,14 +5,58 @@ from flask_cors import CORS, cross_origin
 # import datetime
 # import os
 from config import Config
+from data import response_messages
 
 app = Flask(__name__)
 CORS(app)   # permit all origins
 
+# static data:
+users = [
+    { "username": "admin" } # example struct
+]
+games = [
+    {   # example struct:
+        'id_game': 0,
+        'topic_game': "paises",
+        'username': 'admin',
+        'current_round': 1,
+        'total_correct': 0,
+        'total_errors': 0
+    }
+]
+#####################################################
+
 @app.route("/", methods = ["GET"])
 def index():
-    return "Trivia"
+    return render_template("index.html")
+    # return "Trivia"
 
+app.route("/new_game_<username>", methods = ["GET"])
+def new_game(username):
+    try:
+        for user in users:
+            if user.get('name') == username:
+                message = response_messages.msgs.get('username_not_available')
+                return json.dumps({ 'status': False, 'message': message })
+        # add user:
+        users.append({ 'name': username })
+        return json.dumps({ "status": True, "message": message })
+    except Exception as e:
+        print(e)
+        return json.dumps({ "status": False, "message": "Error interno del servidor" })
+    pass
+
+# return css and static files:
+@app.route('/public/<path:path>')
+def send_css_and_media(path):
+    return send_from_directory('public', path)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "404 not found"
+    # return render_template("404errorPage.html")
+
+# RUN:
 def test():
     app.run(debug=True)
 
---
+++ data/response_messages.py
@@ -0,0 +1,5 @@
+# response messages in api requests
+msgs = {
+    "username_not_available": "Error: Ya existe un usuario con el mismo nombre. Elija otro nombre!",
+    "": ""
+}
